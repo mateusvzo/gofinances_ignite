8f178389336a0df13e20b96d515b979ac54f3d90
mateusvzo <mcarvalhoveloso@hotmail.com>
2021-08-31 14:39:04 -0300

    Add_Theme
    Add Global styles themes

--- src/Pages/Dashboard/styles.ts
+++ src/Pages/Dashboard/styles.ts
@@ -4,11 +4,12 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #5636D3;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
   font-size: 24px;
   font-weight: bold;
-  color: #FFFFFF;
+  color: ${({ theme }) => theme.colors.title};
 `;
