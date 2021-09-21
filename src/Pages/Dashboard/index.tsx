3b2ed3002762cfba898237445dfe4b4e43614164
mateusvzo <mcarvalhoveloso@hotmail.com>
2021-09-21 06:46:57 -0300

    type add in highlightcards

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -35,9 +35,25 @@ export function Dashboard() {
       </Header>
 
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard
+          type= "up" 
+          title="Entradas" 
+          amount="R$ 17.400,00" 
+          lastTransaction="Ultima entrada dia 13 de abril"
+        />
+        <HighlightCard 
+          type= "down" 
+          title="Saídas" 
+          amount="R$ 1.259,00" 
+          lastTransaction="Ultima entrada dia 03 de abril"
+        />
+        <HighlightCard
+          type= "total" 
+          title="Total" 
+          amount="R$ 16.141,00" 
+          lastTransaction="01 à 16 de abril"
+        />
+        
       </HighlightCards>
 
     </Container>
